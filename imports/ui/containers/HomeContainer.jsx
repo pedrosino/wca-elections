85861c045d64ae8c96aa372a4870c4d1cea7e5fa
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-05-19 01:07:25 -0700

    Demo of dead simple usage of WCA api.

--- imports/ui/containers/HomeContainer.jsx
+++ imports/ui/containers/HomeContainer.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 
-const HomePage = ({}) => (
-  <div className="container">
-    Hello, homepage!
-  </div>
-);
+const HomePage = ({user}) => {
+  let userDataDiv = null;
+  if(user) {
+    let wcaUser = user.services.worldcubeassociation;
+    userDataDiv = (
+      <div>
+        {wcaUser.name} {wcaUser.wca_id}
+      </div>
+    );
+  }
+  return (
+    <div className="container">
+      Hello, homepage!
+      {userDataDiv}
+    </div>
+  );
+};
 
 export default HomeContainer = createContainer(props => {
   return {
