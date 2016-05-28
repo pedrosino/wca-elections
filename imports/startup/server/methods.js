d5d33ba8d7f6d20f5bea04dbb22c8be181c9da77
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-05-28 15:31:58 -0700

    Added permissions checking.

--- imports/startup/server/methods.js
+++ imports/startup/server/methods.js
@@ -1,7 +1,10 @@
 import { Votes } from '/imports/collections';
+import { throwUnlessUser, canVote } from '/imports/permissions';
 
 Meteor.methods({
   'vote' (vote) {
+    throwUnlessUser(canVote, this.userId);
+
     console.log(this.userId, vote)
     Votes.upsert({
       userId: this.userId,
