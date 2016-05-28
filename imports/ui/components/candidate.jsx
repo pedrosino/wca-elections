e10f6b9a5a79be8e1ac56cc1b70c5770684c0268
Caleb Hoover <choover11@gmail.com>
2016-05-28 14:41:04 -0700

    now they're button groups

--- imports/ui/components/candidate.jsx
+++ imports/ui/components/candidate.jsx
@@ -24,7 +24,7 @@ class Candidate extends React.Component {
     return (
       <div style={{padding: '10px'}}>
         <h3>{this.props.name}</h3>
-        <div class='btn-group' role='group' aria-label='...'>
+        <div className='btn-group' role='group' aria-label='...'>
           <button type='button' className={`btn btn-default ${this.state.vote === 1 ? 'active' : ''}`} onClick={() => this.vote(1)}>Yes</button>
           <button type='button' className={`btn btn-default ${this.state.vote === 0 ? 'active' : ''}`} onClick={() => this.vote(0)}>Neutral</button>
           <button type='button' className={`btn btn-default ${this.state.vote === -1 ? 'active' : ''}`} onClick={() => this.vote(-1)}>No</button>
