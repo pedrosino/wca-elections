b213c33db9913a3923890b8bc44e703439ea0e92
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-05-29 11:13:21 -0700

    Green buttons for selected preference.

--- imports/ui/components/candidate.jsx
+++ imports/ui/components/candidate.jsx
@@ -13,9 +13,9 @@ class Candidate extends React.Component {
       <div style={{padding: '10px'}}>
         <h3>{this.props.candidate.name}</h3>
         <div className='btn-group' role='group' aria-label='...'>
-          <button type='button' className={`btn btn-default ${this.props.preference === 'yes' ? 'active' : ''}`} onClick={() => this.vote('yes')}>Yes</button>
-          <button type='button' className={`btn btn-default ${this.props.preference === 'neutral' ? 'active' : ''}`} onClick={() => this.vote('neutral')}>Neutral</button>
-          <button type='button' className={`btn btn-default ${this.props.preference === 'no' ? 'active' : ''}`} onClick={() => this.vote('no')}>No</button>
+          <button type='button' className={`btn ${this.props.preference === 'yes' ? 'active btn-success' : 'btn-default'}`} onClick={() => this.vote('yes')}>Yes</button>
+          <button type='button' className={`btn ${this.props.preference === 'neutral' ? 'active btn-success' : 'btn-default'}`} onClick={() => this.vote('neutral')}>Neutral</button>
+          <button type='button' className={`btn ${this.props.preference === 'no' ? 'active btn-success' : 'btn-default'}`} onClick={() => this.vote('no')}>No</button>
         </div>
       </div>
     );
