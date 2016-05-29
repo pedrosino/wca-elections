70747dead01a038505f66ff0c5c7aadf927699f9
Jeremy Fleischman <jeremyfleischman@gmail.com>
2016-05-29 12:40:19 -0700

    Don't allow selecting an invalid preference.

--- imports/startup/server/methods.js
+++ imports/startup/server/methods.js
@@ -1,9 +1,16 @@
 import { Votes } from '/imports/collections';
 import { throwUnlessUser, canVote } from '/imports/permissions';
 
+const ALLOWED_PREFERENCES = ["yes", "no", "neutral"];
+
 Meteor.methods({
   'vote' ({candidateId, preference}) {
     throwUnlessUser(canVote, this.userId);
+
+    if(ALLOWED_PREFERENCES.indexOf(preference) === -1) {
+      throw new Meteor.Error(400,
+          `Preference ${preference} unrecognized. Must be one of ${ALLOWED_PREFERENCES}`);
+    }
 
     let vote = Votes.findOne({
       userId: this.userId
